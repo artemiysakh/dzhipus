c4766874b99b593f88644dfce453cbbcd754bcff
artemiysakh <135399208+artemiysakh@users.noreply.github.com>
2025-08-18 00:08:51 +1100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,12 +20,11 @@ const app=express()
 app.set('trust proxy', 1);
 
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
 app.use(cors({
   origin: [
-    'http://localhost:3000',
-    'https://your-subdomain.trycloudflare.com' // Замените на ваш домен
+     process.env.CLIENT_UR
   ],
   credentials: true
 }));
