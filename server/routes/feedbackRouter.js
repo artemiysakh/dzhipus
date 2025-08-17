8da48bce1b2e085cc59a37ec72e17aeb02fd8573
artemiysakh <135399208+artemiysakh@users.noreply.github.com>
2025-08-18 10:51:50 +1100

    Delete server directory

--- server/routes/feedbackRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const feedbackController = require('../controllers/feedbackController')
-
-router.post('/', feedbackController.create) 
-router.get('/', feedbackController.getFeedbacks)
-router.delete('/:id', feedbackController.deleteFeedback)
-
-module.exports = router
