fd79b11a793b021860467ddd5b679431c16a910f
artemiysakh <artemiysakh0310@gmail.com>
2025-08-25 23:19:26 +1100

    HTTP Commit

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -3,6 +3,8 @@ import { AuthResponse } from '../models/response/AuthResponse';
 
 export const API_URL =  process.env.REACT_APP_API_URL || 'https://dzhipusserver-production.up.railway.app/api'
 
+console.log(API_URL)
+
 const $api = axios.create({
     withCredentials: true,
     baseURL: API_URL
